122fdd0fe075e9275f9413a08c0768cf70bf30eb
erados <imdiots@gmail.com>
2020-12-21 23:51:23 +0900

    yeah

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h1>alsjdflk</h1>
+
+    </div>
+  );
+}
+
+export default App;
